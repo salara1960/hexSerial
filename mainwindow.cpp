d3fb9dd215ea585a08be94f9431b22e754db3f73
salara1960 <a.ilminsky@gmail.com>
2019-10-14 21:14:56 +0200

    Major action : add all files

---
+++ mainwindow.cpp
@@ -0,0 +1,431 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "settingsdialog.h"
+
+
+
+//******************************************************************************************************
+//
+//       hex Serial Terminal
+//
+//******************************************************************************************************
+
+//const QString vers = "0.1";//10.10.2019
+//const QString vers = "0.2";//11.10.2019
+//const QString vers = "0.3";//12.10.2019
+const QString vers = "0.4";//14.10.2019
+
+
+const QString title = "hexSerialTerminal";
+
+
+//******************************************************************************************************
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow), conf(new SettingsDialog)
+{
+    ui->setupUi(this);
+
+    MyError = 0;
+
+    if (!conf) {
+        MyError |= 4;//create settings object error
+        throw TheError(MyError);
+    }
+
+    this->setWindowIcon(QIcon("png/main.png"));
+
+    this->setWindowOpacity(0.90);//set the level of transparency
+
+    this->setTrayIconActions();
+    this->showTrayIcon();
+
+    first  = true;
+    con = false;
+    sdevName = "";
+    sdev = nullptr;
+
+    tmr_sec = startTimer(1000);// 1 sec.
+    if (tmr_sec <= 0) {
+        MyError |= 2;//start_timer error
+        throw TheError(MyError);
+    }
+
+    connect(this, SIGNAL(sigWrite(QByteArray &)), this, SLOT(slotWrite(QByteArray &)));
+
+    //Menu
+    connect(ui->actionVERSION,    &QAction::triggered, this, &MainWindow::About);
+    connect(ui->actionPORT,       &QAction::triggered, conf, &SettingsDialog::show);
+    connect(ui->actionCONNECT,    &QAction::triggered, this, &MainWindow::on_connect_clicked);
+    connect(ui->actionDISCONNECT, &QAction::triggered, this, &MainWindow::on_disconnect_clicked);
+    connect(ui->actionCLEAR,      &QAction::triggered, this, &MainWindow::clrLog);
+
+
+    ui->disconnect->setEnabled(false);
+    ui->connect->setEnabled(true);
+
+    ui->actionCONNECT->setEnabled(true);
+    ui->actionPORT->setEnabled(true);
+    ui->actionDISCONNECT->setEnabled(false);
+
+    ui->status->clear();
+    ui->pic->setPixmap(QPixmap(":png/dis.png"));
+
+}
+//-----------------------------------------------------------------------
+MainWindow::~MainWindow()
+{
+    deinitSerial();
+    if (conf) delete conf;
+
+    killTimer(tmr_sec);
+    delete ui;
+}
+//--------------------------------------------------------------------------------
+
+MainWindow::TheError::TheError(int err) { code = err; }//error class descriptor
+
+//--------------------------------------------------------------------------------
+void MainWindow::clrLog()
+{
+    ui->log->clear();
+}
+//--------------------------------------------------------------------------------
+int MainWindow::initSerial()
+{
+    deinitSerial();
+
+    sdev = new QSerialPort(sdevName);
+    if (sdev) {
+        const SettingsDialog::Settings p = conf->settings();
+        sdevName = p.name;   sdev->setPortName(sdevName);
+        sdev->setBaudRate(p.baudRate);
+        sdev->setDataBits(p.dataBits);
+        sdev->setParity(p.parity);
+        sdev->setFlowControl(p.flowControl);
+        sdev->setStopBits(p.stopBits);
+
+        if (!sdev->open(QIODevice::ReadWrite)) {
+            delete sdev;
+            sdev = nullptr;
+            return 1;
+        } else {
+            rxData.clear();            
+            while (!sdev->atEnd()) rxData.append(sdev->readAll());
+            rxData.clear();
+            txData.clear();
+            hex = true;
+
+            ui->log->append("\n");
+
+            connect(sdev, &QSerialPort::readyRead, this, &MainWindow::ReadData);
+            connect(sdev, &QSerialPort::errorOccurred, this, &MainWindow::slotError);
+
+            return 0;
+        }
+    } else {
+        MyError |= 1;//create serial_port_object error
+        throw TheError(MyError);
+    }
+
+}
+//--------------------------------------------------------------------------------
+void MainWindow::deinitSerial()
+{
+    if (sdev) {
+        if (sdev->isOpen()) sdev->close();
+        sdev->disconnect();
+        delete sdev;
+        sdev = nullptr;
+    }
+}
+//--------------------------------------------------------------------------------
+void MainWindow::About()
+{
+    QString st ="\nSerial port util version " + vers + "\nused : Qt v.";
+    st.append(QT_VERSION_STR);
+
+    QMessageBox box;
+    box.setStyleSheet("background-color: rgb(208, 208, 208);");
+    box.setIconPixmap(QPixmap(":png/salara.png"));
+    box.setText(st);
+    box.setWindowTitle("About");
+    box.exec();
+}
+//-----------------------------------------------------------------------
+void MainWindow::LogSave(const char *func, const QByteArray & st, bool rxd, bool pr)
+{
+    QString fw;
+    if (pr) {
+        time_t ict = QDateTime::currentDateTime().toTime_t();
+        struct tm *ct = localtime(&ict);
+        fw.sprintf("%02d.%02d.%02d %02d:%02d:%02d | ", ct->tm_mday, ct->tm_mon+1, ct->tm_year+1900, ct->tm_hour, ct->tm_min, ct->tm_sec);
+    }
+    if (func) {
+        fw.append("[");
+        fw.append(func);
+        fw.append("] ");
+    }
+    int len = st.length();
+    fw.append(" (" + QString::number(len, 10) + ")");
+    if (rxd) fw.append("> ");
+        else fw.append("< ");
+    if (hex) fw.append(st.toHex(' '));
+        else fw.append(st);
+
+    ui->log->append(fw);//to log screen
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_connect_clicked()
+{
+    if (con) return;
+
+
+    if (!initSerial()) {
+        ui->status->clear();
+        ui->status->setText(tr("Connected to %1 : %2 %3%4%5 FlowControl %6")
+                          .arg(sdevName)
+                          .arg(conf->settings().stringBaudRate)
+                          .arg(conf->settings().stringDataBits)
+                          .arg(conf->settings().stringParity.at(0))
+                          .arg(conf->settings().stringStopBits)
+                          .arg(conf->settings().stringFlowControl));
+        ui->pic->setPixmap(QPixmap(":png/con.png"));
+
+        ui->connect->setEnabled(false);
+        ui->disconnect->setEnabled(true);
+
+        con = true;
+        ui->actionCONNECT->setEnabled(false);
+        ui->actionPORT->setEnabled(false);
+        ui->actionDISCONNECT->setEnabled(true);
+    } else {
+        ui->status->clear();
+        ui->status->setText("Serial port " + sdevName + " open ERROR");
+        ui->pic->setPixmap(QPixmap(":png/dis.png"));
+
+        deinitSerial();
+    }
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_disconnect_clicked()
+{
+    if (!con) return;
+
+    deinitSerial();
+
+    ui->pic->setPixmap(QPixmap(":png/dis.png"));
+    ui->status->clear();
+
+    ui->connect->setEnabled(true);
+    ui->actionPORT->setEnabled(true);
+    ui->disconnect->setEnabled(false);
+
+    con = false;
+    ui->actionCONNECT->setEnabled(true);
+    ui->actionDISCONNECT->setEnabled(false);
+}
+//-----------------------------------------------------------------------
+void MainWindow::slotWrite(QByteArray & mas)
+{
+    if (!sdev) return;
+
+    if (sdev->isOpen()) {
+        if (hex) {
+            sdev->write(mas);
+        } else {
+            QString m(mas);
+            sdev->write(m.toLocal8Bit());
+        }
+
+        LogSave(nullptr, mas, false, true);
+    }
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_ack_clicked()
+{
+    QByteArray m;
+    m.clear();
+    m[0] = ACK;
+    hex = true;
+    emit sigWrite(m);
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_nak_clicked()
+{
+    QByteArray m;
+    m.clear();
+    m[0] = NAK;
+    hex = true;
+    emit sigWrite(m);
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_enq_clicked()
+{
+    QByteArray m;
+    m.clear();
+    m[0] = ENQ;
+    hex = true;
+    emit sigWrite(m);
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_eot_clicked()
+{
+    QByteArray m;
+    m.clear();
+    m[0] = EOT;
+    hex = true;
+    emit sigWrite(m);
+}
+//-----------------------------------------------------------------------
+void MainWindow::on_answer_clicked()
+{
+//0x02, 0x55, 0x00, 0x00, 0x03, 0x56
+//02 55 00 a0 03 f6
+
+    QByteArray sb(ui->stx->text().toLocal8Bit());
+    QByteArray tmp; tmp.clear();
+
+    if (ui->hexBox->checkState() == Qt::Checked) {
+        hex = true;
+        sb = sb.toUpper();
+        hexTobin(sb.data(), &tmp);
+    } else {
+        hex = false;
+        tmp = sb;
+    }
+
+    if (tmp.length()) emit sigWrite(tmp);
+
+}
+//------------------------------------------------------------------------------------
+unsigned char MainWindow::myhextobin(const char *uk)
+{
+char line[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+unsigned char a = 0, b = 0, c = 0, i;
+
+
+    for	(i = 0; i < 16; i++) { if (*uk == line[i]) { b = i; break; } else b = 0xff; }
+    for	(i = 0; i < 16; i++) { if (*(uk + 1) == line[i]) { c = i; break; } else c = 0xff; }
+    if ((b == 0xff) || (c == 0xff)) a = 0xff; else { b <<= 4;   a = b | c; }
+
+    return a;
+}
+//-----------------------------------------------------------------------
+void MainWindow::hexTobin(const char *in, QByteArray *out)
+{
+size_t i = 0, len = strlen(in) / 2;
+const char *uk = in;
+
+    while (i < len) {
+        out->append( static_cast<char>(myhextobin(uk)) );
+        i++; uk += 2;
+    }
+}
+//-----------------------------------------------------------------------
+void MainWindow::timerEvent(QTimerEvent *event)
+{ 
+    if (tmr_sec == event->timerId()) {
+        time_t it_ct = QDateTime::currentDateTime().toTime_t();
+        struct tm *ctimka = localtime(&it_ct);
+        QString dt;
+        dt.sprintf("%02d.%02d.%04d %02d:%02d:%02d",
+                    ctimka->tm_mday,
+                    ctimka->tm_mon + 1,
+                    ctimka->tm_year + 1900,
+                    ctimka->tm_hour,
+                    ctimka->tm_min,
+                    ctimka->tm_sec);
+        setWindowTitle(title +" ver. " + vers + "  |  " + dt);
+    }
+}
+//-----------------------------------------------------------------------
+void MainWindow::ReadData()
+{
+    while (!sdev->atEnd()) rxData.append(sdev->read(1));
+
+    if (rxData.length()) {
+        hex = true;
+        LogSave(nullptr, rxData, true, true);
+        rxData.clear();
+    }
+
+}
+//-----------------------------------------------------------------------
+void MainWindow::slotError(QSerialPort::SerialPortError serialPortError)
+{
+    if (serialPortError == QSerialPort::ReadError) {
+        MyError |= 8;//error reading from serial port
+        throw TheError(MyError);
+    }
+}
+
+//**************************************************************************************
+//                            Tray
+//**************************************************************************************
+void MainWindow::showTrayIcon()
+{
+    trayIcon = new QSystemTrayIcon(this);
+    QIcon trayImage("png/main.png");
+    trayIcon->setIcon(trayImage);
+    trayIcon->setContextMenu(trayIconMenu);
+
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+                this, SLOT(trayIconActivated(QSystemTrayIcon::ActivationReason)));
+
+    trayIcon->show();
+}
+//-------------------------------------------------------------------------------------
+void MainWindow::trayActionExecute()
+{
+    //QMessageBox::information(this, "TrayIcon", "Тестовое сообщение. Замените вызов этого сообщения своим кодом.");
+}
+//-------------------------------------------------------------------------------------
+void MainWindow::trayIconActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch (static_cast<int>(reason)) {
+        case QSystemTrayIcon::Trigger:
+        case QSystemTrayIcon::DoubleClick:
+            this->showNormal();
+        break;
+    }
+}
+//-------------------------------------------------------------------------------------
+void MainWindow::setTrayIconActions()
+{
+    minA  = new QAction(QIcon("png/hide.png"), "Hide", this);
+    maxA  = new QAction(QIcon("png/show.png"), "Show", this);
+    quitA = new QAction(QIcon("png/close.png"),"Quit", this);
+
+
+    connect(minA, SIGNAL(triggered()),  this, SLOT(hide()));
+    connect(maxA, SIGNAL(triggered()),  this, SLOT(showNormal()));
+    connect(quitA, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(minA);
+    trayIconMenu->addAction(maxA);
+    trayIconMenu->addAction(quitA);
+    //trayIconMenu->setStyleSheet(QString::fromUtf8("background-color: rgb(100, 100, 100);"));
+}
+//-------------------------------------------------------------------------------------
+void MainWindow::changeEvent(QEvent *event)
+{
+    QMainWindow::changeEvent(event);
+    if (event->type() == QEvent::WindowStateChange) {
+        if (isMinimized()) this->hide();
+    }
+}
+//-------------------------------------------------------------------------------------
+void MainWindow::closeEvent(QCloseEvent *evt)
+{
+    if (trayIcon->isVisible()) {
+        this->hide();
+        evt->ignore();
+    }
+}
+//**************************************************************************************
+
+
+
+
---
+++ mainwindow.h
@@ -0,0 +1,129 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <inttypes.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <time.h>
+#include <endian.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QByteArray>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QFile>
+#include <QDateTime>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+//#include <QtSerialPort/QSerialPort>
+//#include <QtSerialPort/QSerialPortInfo>
+#include <QDebug>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QCheckBox>
+#include <QSystemTrayIcon>
+#include <QCloseEvent>
+
+
+//********************************************************************************
+
+#define SET_DEBUG
+
+#define max_buf 2048
+
+#define STX  0x02
+#define ETX  0x03
+#define EOT  0x04
+#define ENQ  0x05
+#define ACK  0x06
+#define DLE  0x10
+#define NAK  0x15
+#define ESC  0x1B
+
+
+//********************************************************************************
+
+namespace Ui {
+class MainWindow;
+}
+
+class SettingsDialog;
+
+//********************************************************************************
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+    class TheError {
+        public :
+            int code;
+            TheError(int);
+    };
+
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void timerEvent(QTimerEvent *event);
+
+public slots:
+    int initSerial();
+    void deinitSerial();
+    void LogSave(const char *, const QByteArray &, bool, bool);
+    void About();
+    //void Config();
+    unsigned char myhextobin(const char *);
+    void hexTobin(const char *, QByteArray *);
+    void clrLog();
+    //
+    //tray
+    void changeEvent(QEvent*);
+    void closeEvent(QCloseEvent *);
+    void trayIconActivated(QSystemTrayIcon::ActivationReason reason);
+    void trayActionExecute();
+    void setTrayIconActions();
+    void showTrayIcon();
+    //
+
+private slots:
+    void ReadData();
+    void slotError(QSerialPort::SerialPortError);
+    void on_ack_clicked();
+    void on_nak_clicked();
+    void on_enq_clicked();
+    void on_eot_clicked();
+    void on_answer_clicked();
+
+    void on_connect_clicked();
+    void on_disconnect_clicked();
+
+    void slotWrite(QByteArray &);
+
+signals:
+    void sigWrite(QByteArray &);
+    void sigAbout();
+
+private:
+    Ui::MainWindow *ui;
+    int tmr_sec, MyError;
+    QSerialPort *sdev;
+    QByteArray rxData, txData;
+    QString sdevName, sdevConf;
+    bool hex, first, con;
+    //settings
+    SettingsDialog *conf = nullptr;
+    //tray
+    QMenu *trayIconMenu;
+    QAction *minA;
+    QAction *maxA;
+    QAction *quitA;
+    QSystemTrayIcon *trayIcon;
+};
+
+#endif // MAINWINDOW_H
